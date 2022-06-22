1a826ec6439919a53275087b657eb620ae946aeb
Ngaicode <mjombakevin376@gmail.com>
2022-06-22 13:41:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+"use strict";
+// class of dog
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+// class of cat
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (this.sex == "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
